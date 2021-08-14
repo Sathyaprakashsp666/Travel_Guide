2bdbea05cd20a9a64ec4d90598661611d313ac59
Sathya Prakasha P <sathyaprakash.sp.6@gmail.com>
2021-08-14 13:25:45 +0530

    Header added

--- main/src/components/List/List.jsx
+++ main/src/components/List/List.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import { mergeClasses } from "@material-ui/styles";
+import useStyles from "./styles";
 
 const List = () => {
-    return (
-        <div>
-            List
-        </div>
-    )
-}
+  const classes = useStyles();
 
-export default List
+  const [type, setType] = useState("restaurents");
+  const [rating, setRating] = useState("");
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurents, Hotels & Attractions around you
+      </Typography>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurents">Restaurents</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
+
+export default List;
